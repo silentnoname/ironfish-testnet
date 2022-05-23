06d0540e0f96abde1c9c42897759f18d22cd1187
silentnoname <44841346+silentnoname@users.noreply.github.com>
2022-05-24 00:01:06 +0800

    Update ironfish-node.sh

--- ironfish-node.sh
+++ ironfish-node.sh
@@ -2,4 +2,4 @@ sudo apt-get update &&sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y;
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get update &&  sudo apt-get install docker-ce -y 
-docker run -d --name node --restart--always --tty --interactive --network host --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest
+docker run -d --name node --restart=always --tty --interactive --network host --volume $HOME/.ironfish:/root/.ironfish ghcr.io/iron-fish/ironfish:latest
